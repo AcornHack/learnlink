1b607c79c7d7d0e3cc19313d6db259f6888bd5e4
Stanislav003 <St4nislav003@gmail.com>
2016-11-20 14:09:02 +0000

    stan changes

--- main_app/js/rightAnswers.js
+++ main_app/js/rightAnswers.js
@@ -5,40 +5,47 @@ var rightAnswers = {
     correct: false,
     URL: [" http://www.basic-mathematics.com/multiplying-whole-numbers.html", " https://www.khanacademy.org/math/arithmetic-home/multiply-divide/mult-intro/v/basic-multiplication"]
   },
+// basic multiplication
   question2: {
     title: "13+158+(7x2)",
     answer: "185",
     correct: false,
     URL: [" http://www.bbc.co.uk/bitesize/ks3/maths/number/order_operation/revision/2/", " https://www.youtube.com/watch?v=Ei1UHOFC5OM"]
   },
+//BIDMAS with A M
   question3: {
     title: "(32/4)x3",
     answer: "24",
     correct: false,
     URL: [" http://www.bbc.co.uk/bitesize/ks3/maths/number/order_operation/revision/2/", " https://www.youtube.com/watch?v=Ei1UHOFC5OM"]
   },
+  //BIDMAS * /
   question4: {
     title: "(1x0)x7",
     answer: "0",
     correct: false,
     URL: ["http://www.bbc.co.uk/bitesize/ks3/maths/number/order_operation/revision/2/", " https://www.youtube.com/watch?v=Ei1UHOFC5OM"]
   },
+  // Multiplication
   question5: {
     title: "15x15",
     answer: "225",
     correct: false,
     URL: [" http://www.math.com/school/subject1/lessons/S1U1L9GL.html", " http://study.com/academy/lesson/what-is-a-square-number-in-math-definition-examples.html"]
   },
+  //Squaring numbers
   question6: {
     title: "177x175",
     answer: "30,975",
     correct: false,
     URL: ["https://www.mathsisfun.com/numbers/multiplication-long.html", " http://study.com/academy/lesson/how-to-multiply-large-numbers-steps-and-examples.html"]
   },
+  //LargeMultiplication
   question7: {
     title: "99-18.5",
     answer: "80.5",
     correct: false,
     URL: ["http://www.skillsyouneed.com/num/subtraction.html", " https://www.khanacademy.org/math/arithmetic/arith-review-add-subtract/arith-review-basic-add-subtract/v/basic-subtraction"]
   }
+  //Subtraction with Decimals
 };
